9b46d8b8bc3de27403837a6dae689d433bd4ac0c
Lucycan LY <95648904+looSeeCan@users.noreply.github.com>
2022-05-21 08:51:58 -0500

    Last Nights Work
    forgot to push last nights work

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("PERFORM BASIC OPERATIONS ON NUMBERS IN CSHARP\n");
+
+Console.WriteLine("///////////////////////Add two numeric values///////////////////////////////////////////////\n");
+
+int firstNumber  = 12;
+int secondNumber = 7;
+Console.WriteLine($"{firstNumber + secondNumber}");
+
+Console.WriteLine("//////////////////////////////////////////////////////////");
+
+Console.WriteLine($"\n/////////////////////Mix data types to force implicit type conversions\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
+
+string firstName = "Lucycan Ly";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + widgetsSold + " widgets.");
+
+Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+Console.WriteLine($"\n///////////////////////////Attemp a more advanced case of adding numbers and concatenating strings//////////////////\n");
+
+Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");//treats as string
+
+Console.WriteLine("\n//////////////////////////////////////////////////////////////////////////////\n");
+
+Console.WriteLine($"////////////////Add parentheses to make our intention clear to the compiler////////////////////////\n");
+
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");//() order of operations operator
+
+Console.WriteLine($"\nMATH OPERATORS");
